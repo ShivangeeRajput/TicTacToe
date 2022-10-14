84ae4ad3c1dad263a8a4100d3932cad7710337bd
SR <shivangeerajput871@gmail.com>
2022-10-14 10:13:25 +0530

    Initial commit

---
+++ app/src/test/java/com/example/tictactoe/GameLogic.java
@@ -0,0 +1,11 @@
+package com.example.tictactoe;
+
+public class GameLogic {
+
+    private int[][] gameBoard;
+    private int player = 1;
+
+    String[] playerNames = {"Player 1", "Player 2"};
+     private TextView playerTurn;
+
+}
